4d9f5f72f08f0343c2909f6fa2dfef614c3d7cd0
aras-denizhan <denozhan@hotmail.com>
2022-01-25 15:30:20 +0300

    css load fixed.

--- src/main/java/com/example/yeczane/config/WebSecurityConfig.java
+++ src/main/java/com/example/yeczane/config/WebSecurityConfig.java
@@ -12,9 +12,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/css/**").permitAll();
-        http.authorizeRequests().anyRequest().authenticated()
-                .and().formLogin().loginPage("/login").permitAll()
-                .and().logout().permitAll();
+//        http.authorizeRequests().anyRequest().authenticated()
+//                .and().formLogin().loginPage("/login").permitAll()
+//                .and().logout().permitAll();
+        http.authorizeRequests().anyRequest().permitAll();
     }
 }
