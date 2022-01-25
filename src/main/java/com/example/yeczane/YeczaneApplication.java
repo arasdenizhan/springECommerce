f0118eed68211132771172123e913be5f40a44e7
aras-denizhan <denozhan@hotmail.com>
2022-01-25 14:00:19 +0300

    initial commit

---
+++ src/main/java/com/example/yeczane/YeczaneApplication.java
@@ -0,0 +1,13 @@
+package com.example.yeczane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeczaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeczaneApplication.class, args);
+	}
+
+}
