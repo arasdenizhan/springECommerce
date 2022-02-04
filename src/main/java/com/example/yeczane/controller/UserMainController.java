869c8e734da2fc32bb927871d7d54fdeb405c950
aras-denizhan <denozhan@hotmail.com>
2022-02-04 10:41:38 +0300

    UsersDto wrong parameter fixed.

--- src/main/java/com/example/yeczane/controller/UserMainController.java
+++ src/main/java/com/example/yeczane/controller/UserMainController.java
@@ -37,7 +37,7 @@ public class UserMainController {
                 tempUser.getId(),
                 tempUser.getUsername(),
                 tempUser.getPassword(),
-                tempUser.getPassword()
+                tempUser.getEmail()
         );
         model.addAttribute("currentUser", userDto);
         return "profile";
